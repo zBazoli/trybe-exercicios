2680b0ff880fe71809cafda534de257b6273b509
Rodrigo <rodrigobazoli@hotmail.com>
2022-07-21 14:47:56 -0300

    exercicio 6

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio6.js
@@ -0,0 +1,8 @@
+let bichop = "diagonals";
+let result = "diagonals".toLowerCase();
+
+if (result === "diagonals"){
+    console.log("correto");
+}else if (result != "diagonals") {
+    console.log("incorreto");
+}
